57283d609961155d667fc5a4970ad44123d005c9
Chase Geeseman <chase.geeseman@live.com>
2020-11-24 18:52:42 -0500

    Created test data setup file

---
+++ setup/setup_test_data.sql
@@ -0,0 +1,8 @@
+USE voorhees
+INSERT INTO dbo.people (first_name,last_name,active)
+VALUES
+	('Michael','Mignogna',1),
+	('Jason','Ravitz',1),
+	('Harry','Platt',1),
+	('Michelle','Nocito',1),
+	('Jacklyn','Fetbroyt',1)
