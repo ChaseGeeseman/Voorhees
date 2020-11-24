976c3fe67fe593fa586a39b7304fce495153e094
Chase Geeseman <chase.geeseman@live.com>
2020-11-24 17:50:15 -0500

    created election_history setup script

--- setup/setup.sql
+++ setup/setup.sql
@@ -5,5 +5,24 @@ CREATE TABLE people(
     person_id int IDENTITY(1,1) PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    active BIT
+    /*
+    *   1 = serving activly
+    *   0 = not serving
+    */ 
+    active BIT DEFAULT 0
+)
+
+/*
+*   Contains basic election history information
+*/
+CREATE TABLE election_history(
+    primary_key int IDENTITY(1,1) PRIMARY KEY,
+    /*
+    *   Anyone who participates in an election , should have an entry in the "people"
+    *   table listed above.
+    */
+    CONSTRAINT fk_person_id
+        FOREIGN KEY (person_id)
+            REFERENCES people(person_id),
+    election_year DATE,
 )
