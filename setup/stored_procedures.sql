661489130620af25e8013d51e99a9c821c048106
Chase Geeseman <chase.geeseman@live.com>
2020-11-25 03:48:56 -0500

    initial commit of stored procedure setup script

---
+++ setup/stored_procedures.sql
@@ -0,0 +1,55 @@
+CREATE PROCEDURE ins_people(
+        @first_name VARCHAR(MAX),
+        @last_name VARCHAR(MAX), @active BIT
+    )
+AS
+SET NOCOUNT ON
+INSERT INTO dbo.people(first_name, last_name,active)
+VALUES (@first_name, @last_name, @active)
+GO;
+
+CREATE PROCEDURE ins_meeting(
+        @meeting_date DATE,
+        @sunshine_statement VARCHAR(MAX)
+    )
+AS
+SET NOCOUNT ON
+INSERT INTO dbo.meetings(meeting_date, sunshine_statement)
+VALUES(@meeting_date, @sunshine_statement)
+GO;
+
+CREATE PROCEDURE ins_attendance(
+        @meeting_id INT,
+        @person_id INT
+    )
+AS
+SET NOCOUNT ON
+INSERT INTO dbo.attendance(meeting_id, person_id)
+VALUES(@meeting_id, @person_id)
+GO;
+
+CREATE PROCEDURE ins_resolutions(
+        @res_number VARCHAR(7),
+        @where_as VARCHAR(MAX),
+        @now_therefore VARCHAR(MAX) 
+    )
+AS
+SET NOCOUNT ON
+INSERT INTO dbo.resolutions(res_number, where_as,now_therefore)
+VALUES(@res_number, @where_as, @now_therefore)
+GO;
+
+
+CREATE PROCEDURE ins_election_history(
+    @person_id INT,
+    @election_year DATE,
+    @election_party VARCHAR(MAX),
+    @vote_count INT,
+    @vote_percent FLOAT,
+    @election_year DATE
+)
+AS
+SET NOCOUNT ON
+INSERT INTO dbo.election_history(person_id,election_year,election_party,vote_count,vote_percent,election_year)
+VALUES(@person_id,@election_year,@election_party,@vote_count,@vote_percent,@election_year)
+GO;
