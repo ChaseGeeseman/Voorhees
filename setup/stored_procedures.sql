d62f90e403fa74c40f8715dcc1a3d67c08b8c915
Chase Geeseman <chase.geeseman@live.com>
2020-11-29 02:22:39 -0500

    Updated ins_attendance to check for unique

--- setup/stored_procedures.sql
+++ setup/stored_procedures.sql
@@ -63,8 +63,11 @@ BEGIN
         meeting_id
        ,person_id
     )
-    VALUES
-    ( @meeting_id, @person_id );
+    SELECT  @meeting_id,@person_id
+    WHERE   NOT EXISTS
+    (
+        SELECT  @meeting_id FROM dbo.attendance AS a WHERE a.meeting_id = @meeting_id AND a.person_id = @person_id
+    );
 END;
 GO
 
