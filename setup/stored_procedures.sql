3a1b5ac5d4aa9503fb248c3c492ca220d335f3a1
Chase Geeseman <chase.geeseman@live.com>
2020-11-29 02:33:23 -0500

    Added DROP IF EXISTS to all stored procs and updated formatting.

--- setup/stored_procedures.sql
+++ setup/stored_procedures.sql
@@ -5,7 +5,8 @@ GO
 /*
 TODO: Update this to add a default party if none is provided.
 */
-
+DROP PROCEDURE IF EXISTS dbo.ins_people;
+GO
 CREATE PROCEDURE ins_people
 (
     @first_name VARCHAR(MAX)
@@ -28,7 +29,8 @@ GO
 
 
 ------------- meeting -----------------
-
+DROP PROCEDURE IF EXISTS dbo.ins_meeting;
+GO
 CREATE PROCEDURE ins_meeting
 (
     @ins_meeting_date DATE
@@ -42,14 +44,21 @@ BEGIN
         meeting_date
        ,sunshine_statement
     )
-    SELECT @ins_meeting_date
-	WHERE NOT EXISTS (SELECT @ins_meeting_date FROM dbo.meeting AS m WHERE m.meeting_date = @ins_meeting_date)
+    SELECT  @ins_meeting_date
+           ,@ins_sunshine_statement
+    WHERE   NOT EXISTS
+    (
+        SELECT  @ins_meeting_date
+        FROM    dbo.meeting AS m
+        WHERE   m.meeting_date = @ins_meeting_date
+    );
 END;
 GO
 
 
 ------------- attendance -----------------
-
+DROP PROCEDURE IF EXISTS dbo.ins_attendance;
+GO
 CREATE PROCEDURE ins_attendance
 (
     @meeting_id INT
@@ -63,16 +72,21 @@ BEGIN
         meeting_id
        ,person_id
     )
-    SELECT  @meeting_id,@person_id
+    SELECT  @meeting_id
+           ,@person_id
     WHERE   NOT EXISTS
     (
-        SELECT  @meeting_id FROM dbo.attendance AS a WHERE a.meeting_id = @meeting_id AND a.person_id = @person_id
+        SELECT  @meeting_id
+        FROM    dbo.attendance AS a
+        WHERE   a.meeting_id = @meeting_id
+                AND a.person_id = @person_id
     );
 END;
 GO
 
 ------------- resolutions -----------------
-
+DROP PROCEDURE IF EXISTS dbo.ins_resolution;
+GO
 CREATE PROCEDURE ins_resolution
 (
     @res_number VARCHAR(7)
@@ -94,7 +108,8 @@ END;
 GO
 
 ------------- election_history -----------------
-
+DROP PROCEDURE IF EXISTS dbo.ins_election_history;
+GO
 CREATE PROCEDURE ins_election_history
 (
     @person_id INT
@@ -118,7 +133,8 @@ BEGIN
     ( @person_id, @election_year, @election_party, @vote_count, @vote_percent );
 END;
 GO
-
+DROP PROCEDURE IF EXISTS dbo.view_election_history;
+GO
 CREATE PROCEDURE view_election_history
 (
     @person_id INT = NULL
@@ -141,16 +157,16 @@ BEGIN
                               ,eh.person_id
                                 --If not null return specific value
                               ,@person_id)
-			AND @election_year = IIF(@election_year IS NULL
-                                --If Null return all values
-                              ,eh.election_year
-                                --If not null return specific value
-                              ,@election_year)
-			AND @election_party = IIF(@election_year IS NULL
-                                --If Null return all values
-                              ,eh.election_party
-                                --If not null return specific value
-                              ,@election_year)
+            AND @election_year = IIF(@election_year IS NULL
+                                    --If Null return all values
+                                    ,eh.election_year
+                                    --If not null return specific value
+                                    ,@election_year)
+            AND @election_party = IIF(@election_year IS NULL
+                                        --If Null return all values
+                                     ,eh.election_party
+                                        --If not null return specific value
+                                     ,@election_year)
     ORDER BY eh.election_year
             ,eh.vote_count DESC;
 END;
@@ -159,6 +175,8 @@ GO
 
 
 ------------- party -----------------
+DROP PROCEDURE IF EXISTS dbo.ins_party;
+GO
 CREATE PROCEDURE ins_party
 ( @party_name VARCHAR(MAX))
 AS
