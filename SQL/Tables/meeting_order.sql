d962e318eb6d08bc9fda89c9e8cfe683a889c851
Chase Geeseman <chase.geeseman@live.com>
2020-12-01 12:58:35 -0500

    Updated meeting_order table to include an item_type column
    allows us to tell if item is a res, ord, etc

--- SQL/Tables/meeting_order.sql
+++ SQL/Tables/meeting_order.sql
@@ -11,7 +11,13 @@ CREATE TABLE dbo.meeting_order
    ,meeting_id INT NOT NULL
         CONSTRAINT meeting_order_fk
         FOREIGN KEY( meeting_id )REFERENCES dbo.meeting( meeting_id )
-	--This will reference the primary key of the resolution or ordinance 
+    --This will reference the primary key of the resolution or ordinance 
    ,item_id INT NOT NULL
+    /*
+	This tells us the item type
+		1 = resolution 
+		2 = ordinance
+   */
+   ,item_type INT NOT NULL
    ,meeting_order INT NOT NULL
 );
