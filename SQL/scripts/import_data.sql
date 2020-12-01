3c7c1e3281f98ccabaec37ad1df63653f91aa761
Chase Geeseman <chase.geeseman@live.com>
2020-11-30 19:55:51 -0500

    Now adds meeting dates and sunshine statements

--- SQL/scripts/import_data.sql
+++ SQL/scripts/import_data.sql
@@ -41,3 +41,41 @@ cleanup header row. import order is an imported value, not assigned by SQL.
 DELETE  FROM dbo.import_20201109
 WHERE   import_order = 1;
 
+
+
+
+/*
+Loop through the imported results and insert into the appropriate tables
+*/
+
+DECLARE @last_row           INT =
+        (
+            SELECT  MAX(i.import_order)FROM dbo.import_20201109 AS i
+        )
+       ,@current_row        INT = 2
+        --Needed for ins_meeting
+       ,@date_to_insert     DATE
+       ,@sunshine_to_insert VARCHAR(MAX);
+
+WHILE( @current_row <= @last_row )
+BEGIN
+    SELECT  @current_row;
+    --Import order is the table Primary Key so it will always only return 1 number.
+    SET @date_to_insert =
+    (
+        SELECT  i.meeting_date
+        FROM    dbo.import_20201109 AS i
+        WHERE   i.import_order = @current_row
+    );
+    SET @sunshine_to_insert =
+    (
+        SELECT  i.sunshine
+        FROM    dbo.import_20201109 AS i
+        WHERE   i.import_order = @current_row
+    );
+    EXEC dbo.ins_meeting @meeting_date = @date_to_insert
+                        ,@sunshine_statement = @sunshine_to_insert;
+    SET @current_row = @current_row + 1;
+END;
+
+
