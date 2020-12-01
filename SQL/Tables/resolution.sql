6a673a460ac7d9fbc6cb5b3e022e8802281089d5
Chase Geeseman <chase.geeseman@live.com>
2020-12-01 12:19:04 -0500

    ins_resolution added error checking and additional imports

--- SQL/Tables/resolution.sql
+++ SQL/Tables/resolution.sql
@@ -8,8 +8,11 @@ GO
 CREATE TABLE dbo.resolution
 (
     primary_key INT IDENTITY(1, 1) PRIMARY KEY
-   ,res_number VARCHAR(7) NOT NULL
-   ,res_title VARCHAR(MAX) NULL
-   ,where_as VARCHAR(MAX) NOT NULL
-   ,now_therefore VARCHAR(MAX) NOT NULL
+   ,res_meeting_id INT NOT NULL
+   ,res_reading TINYINT NULL
+   ,res_number VARCHAR(MAX) NULL
+   ,res_title VARCHAR(MAX) NOT NULL
+   ,where_as VARCHAR(MAX) NULL
+   ,now_therefore VARCHAR(MAX) NULL
+   ,dated DATE NOT NULL
 );
