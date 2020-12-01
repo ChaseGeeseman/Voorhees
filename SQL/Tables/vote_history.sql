56f8ee834181c559ed8e00a91f07f4fa08e35083
Chase Geeseman <chase.geeseman@live.com>
2020-12-01 13:31:23 -0500

    Restructure of vote_history table

--- SQL/Tables/vote_history.sql
+++ SQL/Tables/vote_history.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS dbo.vote_history;
 GO
 CREATE TABLE dbo.vote_history
 (
-    vote_history_pk INT IDENTITY(1, 1) PRIMARY KEY
-   ,vote_item INT NOT NULL
+    vote_item INT NOT NULL
+   ,vote_item_type INT NOT NULL
    ,voter INT NOT NULL
    ,CONSTRAINT vote_history_fk_voter
         FOREIGN KEY( voter )
