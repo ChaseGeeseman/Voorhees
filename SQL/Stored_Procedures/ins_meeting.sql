0c940672d6b4aac812a7129520a96b8a1edc1da3
Chase Geeseman <chase.geeseman@live.com>
2020-11-30 20:02:07 -0500

    Added duplicate check to ins_meeting

--- SQL/Stored_Procedures/ins_meeting.sql
+++ SQL/Stored_Procedures/ins_meeting.sql
@@ -10,18 +10,51 @@ CREATE PROCEDURE ins_meeting
 AS
 BEGIN
     SET NOCOUNT ON;
-    INSERT INTO dbo.meeting
-    (
-        meeting_date
-       ,sunshine_statement
-    )
-    SELECT  @ins_meeting_date
-           ,@ins_sunshine_statement
-    WHERE   NOT EXISTS
-    (
+    BEGIN TRY
+        IF EXISTS
+        (
+            SELECT  m.meeting_date
+            FROM    dbo.meeting m
+            WHERE   m.meeting_date = @ins_meeting_date
+        )
+        BEGIN
+            RAISERROR(   'A meeting record for %s already exists'   -- Message text.  
+                        ,16                                         -- Severity.  
+                        ,1                                          -- State.
+                        ,@ins_meeting_date                          --Example variable insert, %d for INT types
+                     );
+        END;
+        INSERT INTO dbo.meeting
+        (
+            meeting_date
+           ,sunshine_statement
+        )
         SELECT  @ins_meeting_date
-        FROM    dbo.meeting AS m
-        WHERE   m.meeting_date = @ins_meeting_date
-    );
+               ,@ins_sunshine_statement
+        WHERE   NOT EXISTS
+        (
+            SELECT  @ins_meeting_date
+            FROM    dbo.meeting m
+            WHERE   m.meeting_date = @ins_meeting_date
+        );
+    END TRY
+    BEGIN CATCH
+        DECLARE @ErrorMessage  NVARCHAR(4000)
+               ,@ErrorSeverity INT
+               ,@ErrorState    INT;
+
+        SELECT  @ErrorMessage  = ERROR_MESSAGE()
+               ,@ErrorSeverity = ERROR_SEVERITY()
+               ,@ErrorState    = ERROR_STATE();
+
+        -- Use RAISERROR inside the CATCH block to return error  
+        -- information about the original error that caused  
+        -- execution to jump to the CATCH block.  
+        RAISERROR(   @ErrorMessage  -- Message text.  
+                    ,@ErrorSeverity -- Severity.  
+                    ,@ErrorState    -- State.  
+                 );
+    END CATCH;
+
 END;
 GO
