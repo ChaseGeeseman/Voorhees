f3d74f616c233d419e49a3394f2e614885bf3e9d
Chase Geeseman <chase.geeseman@live.com>
2020-12-02 07:52:35 -0500

    Added meeting_id to ordinance table

--- SQL/Tables/ordinance.sql
+++ SQL/Tables/ordinance.sql
@@ -6,6 +6,7 @@ GO
 CREATE TABLE dbo.ordinance
 (
     primary_key INT IDENTITY(1, 1) PRIMARY KEY
+	,meeting_id INT NOT NULL
    ,ord_number VARCHAR(7) NOT NULL
    ,ord_reading INT NOT NULL
    ,ord_title VARCHAR(MAX) NULL
