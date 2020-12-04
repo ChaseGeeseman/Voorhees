688a1da181d76491de02787a6c134f4b8d734e8e
Chase Geeseman <chase.geeseman@live.com>
2020-12-04 00:07:06 -0500

    file cleanup due to issue caused by visual studio

--- ProjectVoorhees/dbo/Tables/party.sql
+++ ProjectVoorhees/dbo/Tables/party.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE dbo.party
 (
-    party_id INT IDENTITY(1, 1) PRIMARY KEY
+    party_id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL
    ,party_name VARCHAR(50) NOT NULL
 );
--- ProjectVoorhees/dbo/Tables/party_1.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE dbo.party
-(
-    party_id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL
-   ,party_name VARCHAR(50) NOT NULL
-);
